be4ffb925cf0aa5e8065bbcfe399aa140a0c3774
XenosHat <amanis1st@gmail.com>
2025-06-03 19:59:23 +0530

    Update Bing_Safe_Search.js

--- Bing_Safe_Search.js
+++ Bing_Safe_Search.js
@@ -1,17 +1,28 @@
 // ==UserScript==
-// @name         Bing SafeSearch Enforcer
+// @name          Bing safe search enforcer
 // @namespace    Aman
-// @version      1.1
-// @description  Enforces SafeSearch on Bing
-// @match        *://*.bing.com/search*
+// @version      2.0
+// @description  Enforces SafeSearch on all Bing sections
+// @match        *://*.bing.com/*search*
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
 (function () {
     const url = new URL(location.href);
-    if (url.pathname === "/search" && url.searchParams.get("adlt") !== "strict") {
-        url.searchParams.set("adlt", "strict");
-        location.replace(url);
+    const safePaths = [
+        "/search",
+        "/images/search",
+        "/videos/search",
+        "/news/search",        
+        "/shop"
+    ];
+
+    if (safePaths.some(p => url.pathname.startsWith(p))) {
+        const current = url.searchParams.get("adlt");
+        if (current !== "strict") {
+            url.searchParams.set("adlt", "strict");
+            location.replace(url);
+        }
     }
 })();
